15e7ae3d6f8f9da313aa448e37da5e03a9136da0
RishabhM000 <mishrarishab828@gmail.com>
2025-03-19 01:27:58 +0530

    mongoose connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config() ;
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected to mongodb")
+}).catch((err)=>{
+    console.log(err);
+});
 
 const app=express(); 
 
@@ -6,6 +14,3 @@ app.listen(3000 , ()=>{
     console.log("server is running at port 3000")
 });
 //the express port should be diffrent from the default vite port 5173
-
-
-
