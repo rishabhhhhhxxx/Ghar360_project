d3729d6c6227a12230e08959d9fee5a4313203f4
RishabhM000 <mishrarishab828@gmail.com>
2025-03-19 03:28:04 +0530

    creating a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config() ;
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("connected to mongodb")
@@ -13,4 +14,7 @@ const app=express();
 app.listen(3000 , ()=>{
     console.log("server is running at port 3000")
 });
+
+app.use('/api/user',userRouter);
+
 //the express port should be diffrent from the default vite port 5173
