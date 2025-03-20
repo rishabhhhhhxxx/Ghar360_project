d31b87b411655f3f5f50a6c280b8e69f15bfbe9b
RishabhM000 <mishrarishab828@gmail.com>
2025-03-21 01:28:53 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,7 +2,10 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config() ;
+console.log("MongoDB URI:", process.env.MONGO);
+
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("connected to mongodb")
 }).catch((err)=>{
@@ -10,11 +13,13 @@ mongoose.connect(process.env.MONGO).then(()=>{
 });
 
 const app=express(); 
+app.use(express.json());
 
 app.listen(3000 , ()=>{
     console.log("server is running at port 3000")
 });
 
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
 
 //the express port should be diffrent from the default vite port 5173
