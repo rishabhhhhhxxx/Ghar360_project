b7559fc88c6b0c2975a59330d5f8e64ab00031ea
RishabhM000 <mishrarishab828@gmail.com>
2025-05-24 04:38:25 +0530

    fix

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
-import About from "./pages/about";
-import Profile from "./pages/profile";
-import SignIn from "./pages/signin";
-import SignUp from "./pages/signup";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Profile from "./pages/Profile.jsx";
+import SignIn from "./pages/Signin.jsx";
+import SignUp from "./pages/SignUp.jsx";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
-import UpdateListing from "./pages/updateListing";
+import UpdateListing from "./pages/UpdateListing.jsx";
 import Listing from "./pages/Listing";
 import Search from "./pages/Search";
 
